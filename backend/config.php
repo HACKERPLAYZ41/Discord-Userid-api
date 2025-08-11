2b4a4519d94141f7001510627ee5ef649c11255a
HACKERPLAYZ <utkarshhalwai9@gmail.com>
2025-08-11 14:50:44 +0530

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
-define('BOT_TOKEN', 'MTQwNDE4ODY2MzQyODE1NzQ4MA.GfDs5x.QqElqXHqaGSKIV1wSZ2EPNPZkATSGlJiwgqPjA');
-define('BOT_ID', '1404188663428157480'); // e.g. 123456789012345678
+define('BOT_TOKEN', '');
+define('BOT_ID', ''); // e.g. 123456789012345678
 
-$HEALTH_PASSWORD = "shruti@2402";
+$HEALTH_PASSWORD = "";
 
 // OPTIONAL: If you have an OAuth access token with "connections" scope (user token)
 // you may set it here to let the API return connections for that token owner.
@@ -10,6 +10,7 @@ $HEALTH_PASSWORD = "shruti@2402";
 define('CONNECTIONS_TOKEN', ''); // e.g. 'Bearer xxxxxx' or leave empty
 
 
-$BOT_API_BASE = "http://del-in-01-m.blockcode.in:25003"; // Your Pterodactyl bot server
+$BOT_API_BASE = ""; // Your Pterodactyl bot server
 
 ?>
+
